69a0687d91b7335618669584f324b8f143131dce
keith <keithh@protonmail.com>
2022-12-21 17:43:35 -0500

    fixed docker_compile_ipxe.sh and compile_ipxe.sh, added systemd files

--- compile_ipxe.sh
+++ compile_ipxe.sh
@@ -2,55 +2,55 @@
 #based on https://rpi4cluster.com/pxe/ipxe/
 
 scriptname="compile_ipxe.sh"
+working_dir="/tmp/compile_ipxe"
+	#Change to somewhere in /tmp????
 
 #get pre-requisite packages
-apt update && apt install git make gcc binutils
+apt update -qq
+apt install -yqq git make gcc binutils
 #apt install perl mkisofs syslinux liblzma-dev isolinux mtools
 	#not sure if needed
 
-cd ~/
+mkdir -p "$working_dir"
+cd "$working_dir"
+
+#clear ipxe folder if it exists
+if [[ -d "$working_dir/ipxe" ]] ; then
+	rm -r "$working_dir/ipxe"
+fi
+
 git clone git://git.ipxe.org/ipxe.git
 
 
 ### ipxe/src/config/general.h ###
 #Enable functionality I may want
-defineme=( "DOWNLOAD_PROTO_HTTPS" "DOWNLOAD_PROTO_NFS" "SANBOOT_PROTO_ISCSI" "SANBOOT_PROTO_HTTP" "IMAGE_SCRIPT" "IMAGE_EFI" "REBOOT_CMD" "POWEROFF_CMD" "PING_CMD" "IPSTAT_CMD" ) 
+#defineme=( "DOWNLOAD_PROTO_HTTPS" "DOWNLOAD_PROTO_NFS" "SANBOOT_PROTO_ISCSI" "SANBOOT_PROTO_HTTP" "IMAGE_SCRIPT" "IMAGE_EFI" "REBOOT_CMD" "POWEROFF_CMD" "PING_CMD" "IPSTAT_CMD" )
+defineme=( "DOWNLOAD_PROTO_TFTP" "DOWNLOAD_PROTO_HTTP" "DOWNLOAD_PROTO_HTTPS" "DOWNLOAD_PROTO_NFS" "REBOOT_CMD" "POWEROFF_CMD" "PING_CMD" "IPSTAT_CMD" )
 for ivar in "${!defineme[@]}" ; do
-	sed -i 's;.*'"${defineme[$ivar]}"'.*;#define\t'"${defineme[$ivar]}"';' ~/ipxe/src/config/general.h
+	sed -i 's;.*'"${defineme[$ivar]}"'.*;#define\t'"${defineme[$ivar]}"';' "$working_dir/ipxe/src/config/general.h"
 done
 
 #Disable functionality I probably won't want
-undefineme=( "NET_PROTO_STP" "NET_PROTO_EAPOL" "CRYPTO_80211_WEP" "CRYPTO_80211_WPA" "CRYPTO_80211_WPA2" "IMAGE_MULTIBOOT" "IMAGE_DER" "IMAGE_PEM" "VNIC_IPOIB" ) 
+#undefineme=( "NET_PROTO_STP" "NET_PROTO_EAPOL" "CRYPTO_80211_WEP" "CRYPTO_80211_WPA" "CRYPTO_80211_WPA2" "IMAGE_MULTIBOOT" "IMAGE_DER" "IMAGE_PEM" "VNIC_IPOIB" )
+undefineme=( "NET_PROTO_FCOE" "NET_PROTO_STP" "NET_PROTO_LACP" "NET_PROTO_EAPOL" "DOWNLOAD_PROTO_FTP" "DOWNLOAD_PROTO_SLAM" "CRYPTO_80211_WEP" "CRYPTO_80211_WPA" "CRYPTO_80211_WPA2" "IMAGE_PNG" "IMAGE_DER" "IMAGE_PEM" "IWMGMT_CMD" "IBMGMT_CMD" "FCMGMT_CMD" "SANBOOT_CMD" "IMAGE_ARCHIVE_CMD" "VNIC_IPOIB" )
 for ivar in "${!undefineme[@]}" ; do
-	sed -i 's;.*'"${undefineme[$ivar]}"'.*;#undefine\t'"${undefineme[$ivar]}"';' ~/ipxe/src/config/general.h
+	sed -i 's;.*'"${undefineme[$ivar]}"'.*;#undef\t'"${undefineme[$ivar]}"';' "$working_dir/ipxe/src/config/general.h"
 done
 
 #It's not clear to me if '#undef...' and '//#define' have the same effect. The intended effect to save space in ipxe.efi so 'make' works.
 
 ### ipxe/embed.ipxe ###
-#copies the following lines to ~/ipxe/embed.ipxe, excluding the first and last lines (i.e. with EOF in them)
-cat << EOF >~/ipxe/embed.ipxe
-#!ipxe
-dhcp && goto netboot || goto dhcperror
-
-:dhcperror
-prompt --key s --timeout 10000 DHCP failed, hit 's' for the iPXE shell; reboot in 10 seconds && shell || reboot
-
-:netboot
-chain tftp://${next-server}/main.ipxe ||
-prompt --key s --timeout 10000 Chainloading failed, hit 's' for the iPXE shell; reboot in 10 seconds && shell || reboot
-EOF
 #A key part of this embed.ipxe is that you're hard-coding in tftp://${next-server}/main.ipxe as the iPXE script location that ipxe.efi will always try to load
 	#make sure your first script is called main.ipxe, and is in the root of the tftp server that your dhcp server advertises as the "next server"
 
 
 ### compile ipxe.efi ###
-cd ~/ipxe/src
-make bin-x86_64-efi/ipxe.efi EMBED=embed.ipxe
+cd "$working_dir/ipxe/src"
+make bin-x86_64-efi/ipxe.efi EMBED="$working_dir/embed.ipxe"
 
 ### check if ipxe.efi exists and report its full path
-if [[ -f ~/ipxe/src/bin-x86_64-efi/ipxe.efi ]] ; then
-	printf '%s\n' "$scriptname: ipxe exists at $(stat --format=%n ~/ipxe/src/bin-x86_64-efi/ipxe.efi)"
+if [[ -f "$working_dir/ipxe/src/bin-x86_64-efi/ipxe.efi" ]] ; then
+	printf '%s\n' "$scriptname: ipxe exists at $(stat --format=%n $working_dir/ipxe/src/bin-x86_64-efi/ipxe.efi)"
 	printf '%s\n' "$scriptname: success(?)"
 fi
 
--- docker_compile_ipxe.sh
+++ docker_compile_ipxe.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
-#Runs compile-ipxe.sh in a docker container then copies ipxe.efi to the current directory
+#Runs compile-ipxe.sh in a docker container then copies ipxe.efi to the netboot directory
 #Assumes:
 	#you already have docker set up and running, AND use systemd
 		#If you don't, consider running compile-ipxe.sh directly or boot from a live debian ISO and run it there
 	#compile-ipxe.sh is in the current directory
 
 scriptname="docker_compile_ipxe.sh"
+working_dir="/tmp/compile_ipxe/"
+target_path_base="/z/mix/docker/common-netboot/"
 container=tmp-compile-ipxe
-image=library/debian:testing-slim
+image=library/debian:stable-slim
 
 #checks docker is running
 if [[ "$(systemctl is-active docker)" != "active" ]] ; then
@@ -27,31 +29,30 @@ docker run -d \
 	--name=$container \
 	--restart unless-stopped \
         $image bash -c "sleep 360000"
-
+	--cpus 1
 	#move these above if network issues encountered
 	#-h $container \
 	#-e TZ=America/Toronto \
         #--network=$(cat /z/mix/st/t1/a/ip.txt | grep $container | sed 's/.*macvlan/macvlan/' | sed 's/[[:space:]].*//g' ) \
         #--ip=$(cat /z/mix/st/t1/a/ip.txt | grep $container | awk '{print $1}') \
 
-#copy compile-ipxe.sh to container
+#copy compile-ipxe.sh and embed.ipxe to container
+docker exec -it "$container" bash -c "mkdir -p $working_dir"
 docker cp compile_ipxe.sh "$container":/compile_ipxe.sh
+docker cp embed.ipxe "$container":"$working_dir/embed.ipxe"
 
 #run compile-ipxe.sh
-docker exec -it $container bash -c "chmod -R 777 /compile_ipxe.sh && /compile_ipxe.sh"
+docker exec -it $container bash -c "chmod -R 777 /compile_ipxe.sh && /compile_ipxe.sh > /dev/null"
 
 #extract ipxe.efi
-docker cp "$container":/root/ipxe/src/bin-x86_64-efi/ipxe.efi ipxe.efi.new
+docker cp "$container":"$working_dir/ipxe/src/bin-x86_64-efi/ipxe.efi" ipxe.efi.new
 
 #destroy container
 docker rm -f $container
 
-#rename ipxe.efi.new to ipxe.efi
-while [[ -f "ipxe.efi" ]] ; do
-	printf '%s\n' "scriptname: old ipxe.efi exists, WILL OVERWRITE with ipxe.efi.new if script continues"
-	mv -i ipxe.efi.new ipxe.efi
-done
-
+#rename ipxe.efi.new to ipxe.efi and move it in place
+mv ipxe.efi.new "$target_path_base/ipxe.efi"
+chmod 644 "$target_path_base/ipxe.efi"
 
 
 
